5455431012c2c4b8023cbcb7a4b066ff8d6875f1
Aloik1 <ilykond@gmail.com>
2025-03-17 15:34:58 +0100

    a.h norminette good

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: aloiki <aloiki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 10:35:55 by aloiki            #+#    #+#             */
-/*   Updated: 2025/03/17 15:28:22 by aloiki           ###   ########.fr       */
+/*   Updated: 2025/03/17 15:34:34 by aloiki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,27 @@
 
 typedef struct s_philosophers
 {
-	int	id;
-	size_t last_meal;
-	pthread_t thread;
-	struct s_philo *shared_philo;
-	int	times_ate;
-	int	times_slept;
-	int	times_thought;
-	pthread_mutex_t mutex;
-} t_philosophers; 
+	struct s_philo	*shared_philo;
+	pthread_t		thread;
+	size_t			last_meal;
+	int				id;
+	int				times_ate;
+	int				times_slept;
+	int				times_thought;
+	pthread_mutex_t	mutex;
+}	t_philosophers;
 
 typedef struct s_philo
 {
-	int number_of_times_each_philosopher_must_eat;
-	int number_of_philosophers;
-	size_t time_to_die;
-	size_t time_to_eat;
-	size_t time_to_sleep;
-	size_t	start_time;
-	pthread_t monitor_death;
-	pthread_mutex_t 	*forks;
-	t_philosophers		*philosophers;
+	pthread_mutex_t	*forks;
+	pthread_t		monitor_death;
+	t_philosophers	*philosophers;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				number_of_times_each_philosopher_must_eat;
+	int				number_of_philosophers;
 }	t_philo;
 
 #endif
